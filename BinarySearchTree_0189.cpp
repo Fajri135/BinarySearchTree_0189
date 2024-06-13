5e46973a20d25e5227e3199f46fe2ce7fa086c27
Fajri135 <corex135@gmail.com>
2024-06-13 13:45:16 +0700

    Membuat prosedur fungsi insert di dalam class BinaryTree

--- BinarySearchTree_0189.cpp
+++ BinarySearchTree_0189.cpp
@@ -26,6 +26,34 @@ public:
         ROOT = nullptr; //Initializing ROOT to null
     }
 
+    void insert(string element) // insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
+        newNode->info = element; //assign value to the data field of the new node
+        newNode->leftchild = nullptr; //make the left child of the new node point to null
+        newNode->rightchild = nullptr; //make the right child of the newnode point to null
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // locate the node which will be the parent of the node to be insert
+
+        if (parent == nullptr) // if the parent is NULL (tree is empty)
+        {
+            ROOT = newNode; // mark the new node as root
+            return; // exit
+        }
+
+        if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newNode; //make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) //if the value in the data field of the new node is greater than that of the parent
+        {
+            parent->rightchild = newNode; // make  the right child of the parent point to the new node
+        }
+    }
+
+    
 };
 
 int main()
